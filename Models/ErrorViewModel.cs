0a73258213662f92637587f93cf31c36b5fc6846
Shubham Kanujiya <121602512+shubham7354@users.noreply.github.com>
2025-03-06 22:28:15 +0530

    Added .gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace xRootServices.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
