1e32252e49297daa5532bec8e82ee60a69731794
nishant451 <nishantkushwaha451@gmail.com>
2025-05-19 21:41:23 +0530

    nishant changes

---
+++ Controllers/aboutus.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace xRootServices.Controllers
+{
+    public class aboutus : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
